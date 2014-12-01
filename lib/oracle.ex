b7724a5994eab9ea6988a37ce3a5a13ca65d3db4
Tomas Brukner <tomas.brukner@gmail.com>
2014-12-01 15:20:33 +0100

    Oracle has now state.

--- lib/oracle.ex
+++ lib/oracle.ex
@@ -1,5 +1,11 @@
 defmodule Oracle do
-  def edge_time(map, from, to, _timeframe) do
+  defstruct map: nil
+
+  def new(map) do
+    %Oracle{map: map}
+  end
+
+  def edge_time(%Oracle{map: map}, from, to, _timeframe) do
     RoadMap.edges(map, from)
       |> Enum.find(fn {_f, t, _l} -> t == to end)
       |> elem(2)
--- test/oracle_test.exs
+++ test/oracle_test.exs
@@ -3,15 +3,17 @@ defmodule OracleTest do
 
   test "Can give time for segment" do
     m = RoadMap.new("sample_map.txt")
+    o = Oracle.new(m)
 
-    assert Oracle.edge_time(m, "A", "B", 4) == 6
-    assert Oracle.edge_time(m, "A", "B", 0) == 6
+    assert Oracle.edge_time(o, "A", "B", 4) == 6
+    assert Oracle.edge_time(o, "A", "B", 0) == 6
   end
 
   test "Can give time for junction" do
     m = RoadMap.new("sample_map.txt")
+    o = Oracle.new(m)
     
-    assert Oracle.vertex_time(m, "A", "B", "C", 4) == 0
-    assert Oracle.vertex_time(m, "A", "B", "C", 5) == 0
+    assert Oracle.vertex_time(o, "A", "B", "C", 4) == 0
+    assert Oracle.vertex_time(o, "A", "B", "C", 5) == 0
   end
 end
