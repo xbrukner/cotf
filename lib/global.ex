6bbbb434df2c2769ca55d73a079cd1f0b38228dd
Tomas Brukner <tomas.brukner@gmail.com>
2014-12-11 20:33:53 +0100

    Correct single segment calculation.

--- lib/global.ex
+++ lib/global.ex
@@ -1,3 +1,3 @@
 defmodule Global do
-  defstruct oracle: nil, map: nil, planner: nil, timeframe: nil, aggregator: nil
+  defstruct oracle: nil, map: nil, planner: nil, timeframe: nil, aggregator: nil, tf_duration: 0
 end
--- lib/single_delay.ex
+++ lib/single_delay.ex
@@ -1,6 +1,6 @@
 defmodule SingleDelay do
 
-  def segment(global, _number_of_cars, from, to) do
+  def segment(global, number_of_cars, from, to) do
     map = global.map
 
     length = RoadMap.edges(map, from) #Outgoing
@@ -9,12 +9,41 @@ defmodule SingleDelay do
       |> Float.parse #Parse to {float, rest}
       |> elem(0) #Choose float
 
-    fs = 80 #Free flow speed - kph
-#capacity = 1850
-    #MAGIC
+    type = 1 #TODO
+    {_speed, duration} = segment_calculation(global.tf_duration, number_of_cars, length, type)
+    duration
+  end
 
-    time_in_hours = length / fs
-    time_in_hours * 3600
+  def segment_calculation(tf_duration, number_of_cars, length, type) do
+#From table
+    types = %{
+#Free flow speed, capacity
+      1 => {80, 1850},
+      2 => {65, 1800},
+      3 => {55, 1750},
+      4 => {45, 1700}
+    }
+    
+    {vf, c} = types[type]
+    q = number_of_cars * 3600 / tf_duration #veh/hour 
+
+    #Constants
+    x0 = 0.5
+    tf = 0.25
+    k = 2.31
+
+    #Optimization
+    q_c = q / c
+    q_c_1 = q_c - 1
+
+    #MAGIC
+    speed = vf / 
+      (1 + 0.25 * vf * tf *
+       ( q_c_1 + :math.sqrt( q_c_1 * q_c_1 + 8*k*(q_c - x0)/(c*tf) ) )
+      )
+
+    time_in_hours = length / speed
+    {speed, time_in_hours * 3600}
   end
 
   def junction(_global, _number_of_cars, _from, _via, _to) do
