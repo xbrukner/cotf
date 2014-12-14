93995753b58c66891ada53976defe6501b8f82a8
Tomas Brukner <tomas.brukner@gmail.com>
2014-12-14 13:00:05 +0100

    Segment delay calculation.

--- lib/delay.ex
+++ lib/delay.ex
@@ -1,4 +1,5 @@
 defmodule Delay do
+#Junctions
   def junction(global, from, via, cars) do
     delayfn = &(SingleDelay.junction(global, &1, from, via))
     durations = Enum.map(cars, fn({tf, cars}) -> {tf, delayfn.(cars)} end)
@@ -13,6 +14,87 @@ defmodule Delay do
     altered_duration = duration + if overflow > 0 do overflow else 0 end
     
     { {tf, altered_duration}, Dict.put_new(res, tf, altered_duration) }
+  end
+
+#Segments
+  def segment(global, from, to, cars) do
+    calcfn = SingleDelay.segment_calc_fn(global, from, to)
+    speeds_times = Enum.map(cars, fn({tf, cars}) -> {tf, {cars, calcfn.(cars)} } end)
+    length = RoadMap.length(global.map, from, to)
+
+    #Constants
+    car_length = 0.0045 #km
+    safe_distance = 1 # seconds
+    reducefn = &(segment(global.tf_duration, length, {car_length, safe_distance}, &1, &2))
+    Enum.reduce(speeds_times, { {-1, -1, 0, [] }, %{} }, reducefn)
+      |> elem(1)
+  end
+
+# speeds = (km/h, km, seconds) that the car will cover
+  defp segment(tf_duration, length, constants, {tf, {cars, {speed, time}}}, { {l_start_time, l_finish_time, l_cars, l_speeds}, res}) do
+    start_time = tf * tf_duration
+    finish_time = start_time + time
+    if finish_time > l_finish_time do
+      { {start_time, finish_time, cars, [{speed, length, time}] }, Dict.put_new(res, tf, time) }
+    else
+      speeds = catching_up(start_time - l_start_time, constants, speed, l_cars, l_speeds)
+      total_time = Enum.reduce(speeds, 0, fn ({_s, _l, t}, acc) -> t + acc end)
+      { {start_time, start_time + total_time, cars, speeds}, Dict.put_new(res, tf, total_time) }
+    end
+  end
+
+#TODO - calculate queue_length on final speed to set final time as time when the end of queue exits the segment
+#defp queue_length({car_length, safe_distance}, cars, safe_distance) do
+
+  defp catching_up(time_difference, {car_length, safe_distance}, speed, l_cars, l_speeds) do
+#Difference in initial distance
+    distance = distance_from_speeds(l_speeds, time_difference) - car_length * l_cars # - queue length
+    catching_up_distance(distance, safe_distance, speed, l_cars, l_speeds)
+  end
+
+#Cathing up when knowing distance
+  defp catching_up_distance(distance_to_cover, safe_distance, speed, l_cars, l_speeds) do
+    catching_up_distance(safe_distance, speed, l_cars, l_speeds, distance_to_cover, 0, 0)
+  end
+
+#Finished in this segment
+  defp catching_up_distance(safe_distance, m_speed, l_cars, [{speed, length, time} | r_speeds], distance_to_cover, first_time, first_distance)
+    when (m_speed - speed) * time / 3600 >= distance_to_cover - speed * l_cars * safe_distance / 3600 do
+
+    remaining_distance_to_cover = distance_to_cover - speed * l_cars * safe_distance / 3600
+    added_time = remaining_distance_to_cover / (m_speed - speed) * 3600
+    total_distance = (first_time + added_time) / 3600 * m_speed
+
+    remaining_length = length - (total_distance - first_distance)
+    remaining_current = if remaining_length > 0 do
+      [ {speed, remaining_length, remaining_length / speed * 3600} ]
+    else 
+      []
+    end
+
+
+    [ {m_speed, total_distance, first_time + added_time} ] ++
+      remaining_current ++ r_speeds
+  end
+
+  defp catching_up_distance(safe_distance, m_speed, l_cars, [{speed, length, time} | r_speeds],distance_to_cover,  first_time, first_distance) do
+    catching_up_distance(safe_distance, m_speed, l_cars, r_speeds, distance_to_cover - (m_speed - speed) * time / 3600, first_time + time, first_distance + length) 
+  end
+
+
+  defp distance_from_speeds(l_speeds, time_diff) do
+    distance_from_speeds(l_speeds, time_diff, 0)
+  end
+
+#Remaining time is spent in this segment
+  defp distance_from_speeds([{speed, _length, time} | _r_speeds], time_diff, distance)
+    when time_diff <= time do
+    distance + (speed * time_diff / 3600)
+  end
+
+#Not all time is spent in this segment
+  defp distance_from_speeds([{_speed, length, time} | r_speeds], time_diff, distance) do
+    distance_from_speeds(r_speeds, time_diff - time, distance + length)
   end
 end
 
--- test/delay_test.exs
+++ test/delay_test.exs
@@ -9,4 +9,15 @@ defmodule DelayTest do
 #Checked manually, it calls static function now
     assert Delay.junction(g, "A", "B", cars) == %{0 => 61, 1 => 62, 3 => 61}
   end
+
+  test "Segment delay" do
+    m = RoadMap.new("sample_map.txt")
+    g = %Global{map: m, tf_duration: 60}
+    cars = %{0 => 10, 1 => 20, 2 => 30, 3 => 10, 4 => 10}
+
+#Modified to reflect float arithmetics of elixir
+    assert Delay.segment(g, "A", "B", cars) == 
+      #1 => 91->904, 3 => 264->26
+      %{0 => 262.9604253728268, 1 => 281.2766806290091, 2 => 422.65292781515166, 3 => 402.1626186909926, 4 => 334.8422065254472} 
+  end
 end
